e7c0efb38793bc033bf7e4f3e1b5113d75e962a4
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-06-02 18:50:59 -0300

    Prefer specifying only groupbys instead of functions

--- record_summary.py
+++ record_summary.py
@@ -1,8 +1,8 @@
-def amount_by_month(data):
-    """Sums amount grouping by Year-Month"""
-    return data.groupby(data['date'].dt.strftime("%Y-%m")).amount.sum().to_frame()
+def groupby_month(data):
+    """Returns groupby month given data records"""
+    return data['date'].dt.strftime("%Y-%m")
 
 
-def total_amount_by(groupby, records):
+def total_amount_by(groupby, records, ascending=True):
     """Sums total amount given a column to group by"""
-    return records.groupby(groupby).sum().sort_values('amount')
+    return records.groupby(groupby).sum().sort_values('amount', ascending=ascending)
