e973e40db5b51edf09270e8df041b72c2f9cfdad
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-07-07 12:03:26 -0300

    Remove older splitwise exports

--- move_files_to_data.sh
+++ move_files_to_data.sh
@@ -9,5 +9,8 @@ mv $HOME/Downloads/nubank*.csv ./data/
 # Spliwise csv exports from mobile app
 mv $HOME/Downloads/Splitwise*.csv ./data/
 
+# Remove current splitwise exports
+rm ./data/mozi-e-eu*.csv
+
 # Spliwise csv exports from site
 mv $HOME/Downloads/mozi-e-eu*.csv ./data/
