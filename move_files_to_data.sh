54032d7de707db352a89a411f3d9e7f6fead470c
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2018-08-12 16:14:00 -0300

    Make download folder easy to change for moving script

--- move_files_to_data.sh
+++ move_files_to_data.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+DOWNLOAD_FOLDER="$HOME/Downloads"
+
 # Other accounts csv exports
-mv $HOME/Downloads/Extrato\ outras\ contas*.csv ./data/
+mv $DOWNLOAD_FOLDER/Extrato\ outras\ contas*.csv ./data/
 
 # Nubank csv exports
-mv $HOME/Downloads/nubank*.csv ./data/
+mv $DOWNLOAD_FOLDER/nubank*.csv ./data/
 
 # Spliwise csv exports from mobile app
-mv $HOME/Downloads/Splitwise*.csv ./data/
+mv $DOWNLOAD_FOLDER/Splitwise*.csv ./data/
 
 # Remove current splitwise exports only if there is another to move
-if [ -f $HOME/Downloads/mozi-e-eu*.csv ]; then
+if [ -f $DOWNLOAD_FOLDER/mozi-e-eu*.csv ]; then
   rm ./data/mozi-e-eu*.csv
 fi
 
 # Spliwise csv exports from site
-mv $HOME/Downloads/mozi-e-eu*.csv ./data/
+mv $DOWNLOAD_FOLDER/mozi-e-eu*.csv ./data/
