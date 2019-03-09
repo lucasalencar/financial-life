1b12d6782ddd4b43197f95474d9a41a336dd9d36
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-03-09 12:12:38 -0300

    Add fix for empty categories on nubank files

--- load/nubank.py
+++ load/nubank.py
@@ -4,9 +4,18 @@ All Nubank functions necessary to load and preprocess its data
 from load import read
 
 
+def fix_empty_category(category):
+    if isinstance(category, str):
+        return category
+    print('Warning: nubank empty category changed to Outros')
+    return 'Outros'
+
+
 def preprocess(expenses):
     # Remove cagegory Pagamento
     expenses = expenses.loc[expenses['category'] != 'Pagamento']
+    expenses = expenses.loc[expenses['title'] != 'Pagamento recebido']
+    expenses['category'] = expenses['category'].apply(fix_empty_category)
     return expenses[['date', 'title', 'category', 'amount']]
 
 
