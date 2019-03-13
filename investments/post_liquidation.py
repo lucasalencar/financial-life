3908f5b726bb51945dd79c74bf2dd6d7fc987355
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-03-12 22:06:11 -0300

    Add post liquidation analysis to investments
    Create a module just to add functions related to post liquidation
    analysis.

---
+++ investments/post_liquidation.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from investments import totals as tt
+
+
+def liquidations(incomes):
+    applications = tt.total_applications_by('title', incomes)
+    return applications[applications.amount <= 0]
+
+
+def liquidations_incomes(incomes):
+    finished = liquidations(incomes).reset_index().title
+    return incomes[incomes.title.isin(list(finished))]
+
+
+def final_return(incomes):
+    applications = tt.total_applications_by('title', incomes[incomes.amount > 0])
+    liquidation_titles = tt.total_income_by('title', incomes)
+    discounts = tt.total_discounts_by('title', incomes)
+    net_liquidations = liquidation_titles\
+        .sub(applications, fill_value=0)\
+        .add(discounts, fill_value=0)
+    net_return_percentage = net_liquidations.div(applications)
+
+    data = {
+        'Applications': applications,
+        'liquidation_titles': liquidation_titles,
+        'Discounts': discounts,
+        'Net liquidations': net_liquidations,
+        'Net Return (%)': net_return_percentage,
+    }
+
+    final_returns = pd.concat(data.values(), axis=1, sort=False)
+    final_returns.columns = data.keys()
+    return final_returns.fillna(0)
