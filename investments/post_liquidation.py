df275ca7826587b2f908fc8d398b4646f57e90df
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-03-17 10:54:40 -0300

    Compute monthly and yearly return for finished investments
    Looking at how much was the return monthly or yearly, helps to
    understand how profitable was the investment during the time the money
    was invested.

--- investments/post_liquidation.py
+++ investments/post_liquidation.py
@@ -12,14 +12,59 @@ def liquidations_incomes(incomes):
     return incomes[incomes.title.isin(list(finished))]
 
 
+def first_application_date(title, incomes):
+    t = incomes[incomes.title == title]
+    return t[t.category == 'aplicação'].sort_values('date').iloc[0].date
+
+
+def liquidation_date(title, incomes):
+    t = incomes[incomes.title == title]
+    return t[t.category == 'renda'].sort_values('date').iloc[0].date
+
+
+def application_months(title, incomes):
+    application_time = liquidation_date(title, incomes) - first_application_date(title, incomes)
+    return application_time.days / 30
+
+
+def application_years(title, incomes):
+    application_time = liquidation_date(title, incomes) - first_application_date(title, incomes)
+    return application_time.days / 365
+
+
+def applications_durations(liquidation_titles, incomes):
+    durations = {'title': [], 'Duration months': [], 'Duration years': []}
+
+    for title in liquidation_titles.index:
+        durations['title'].append(title)
+        durations['Duration months'].append(application_months(title, incomes))
+        durations['Duration years'].append(application_years(title, incomes))
+
+    return pd.DataFrame(durations).set_index('title')
+
+
+def compute_net_return(liquidation_titles, applications, discounts):
+    return liquidation_titles\
+    .sub(applications, fill_value=0)\
+    .add(discounts, fill_value=0)
+
+
+def monthly_return(net_return, durations):
+    return net_return.amount / durations['Duration months']
+
+
+def yearly_return(net_return, durations):
+    applicable_titles = durations[durations['Duration years'] > 1]
+    return net_return.amount / applicable_titles['Duration years']
+
+
 def final_return(incomes):
     applications = tt.total_applications_by('title', incomes[incomes.amount > 0])
     liquidation_titles = tt.total_income_by('title', incomes)
     discounts = tt.total_discounts_by('title', incomes)
-    net_return = liquidation_titles\
-        .sub(applications, fill_value=0)\
-        .add(discounts, fill_value=0)
+    net_return = compute_net_return(liquidation_titles, applications, discounts)
     net_return_percentage = net_return.div(applications)
+    durations = applications_durations(liquidation_titles, incomes)
 
     data = {
         'Applications': applications,
@@ -27,6 +72,10 @@ def final_return(incomes):
         'Discounts': discounts,
         'Net return': net_return,
         'Net return (%)': net_return_percentage,
+        'Duration months': durations['Duration months'],
+        'Return a.m.': monthly_return(net_return, durations),
+        'Duration years': durations['Duration years'],
+        'Return a.y.': yearly_return(net_return, durations),
     }
 
     final_returns = pd.concat(data.values(), axis=1, sort=False)
