ff48cb4f6c9da7a0a443aed3f2cedc1c8f17dd76
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-04-09 21:59:42 -0300

    Add expenses category module

--- expenses/category.py
+++ expenses/category.py
@@ -1,21 +1,20 @@
 import pandas as pd
-import numpy as np
-import seaborn
+
 import formatting as fmt
 import record_summary as rs
 
 
-def expense_distribution(expenses, denominator):
+def distribution_by_category(expenses, denominator):
     denominator_sum = denominator.amount.sum()
     expenses_by_category = rs.total_amount_by('category', expenses).sort_values('amount')
     return expenses_by_category / denominator_sum
 
 
-def describe_expenses(expenses, incomes):
+def distribution(expenses, incomes):
     expenses_by_category = rs.total_amount_by('category', expenses)
-    dist_by_spent = expense_distribution(expenses, expenses)
-    dist_by_income = expense_distribution(expenses,
-                                          incomes[incomes.category == 'renda']) * -1
+    dist_by_spent = distribution_by_category(expenses, expenses)
+    dist_by_income = distribution_by_category(expenses,
+                                              incomes[incomes.category == 'renda']) * -1
 
     return pd.DataFrame({'amount #': expenses_by_category.amount,
                          '% by expenses': dist_by_spent.amount,
@@ -30,23 +29,23 @@ EXPENSES_DISTRIBUTION_COLS_FORMAT = {
 }
 
 
-def style_expenses_distribution(dist):
+def style_distribution(dist):
     return dist.style.format(EXPENSES_DISTRIBUTION_COLS_FORMAT)
 
 
-def _expenses_over_time_(expenses, incomes, post_describe_fn):
+def describe_over_time(expenses, incomes, post_describe_fn):
     """Computes expenses distribution over time, for all months available in expenses"""
     return rs.describe_over_time(expenses,
                               lambda exps, date: post_describe_fn(
-                                  describe_expenses(rs.records_for_month(expenses, date),
-                                                    rs.records_for_month(incomes, date))))
+                                  distribution(rs.records_for_month(expenses, date),
+                                               rs.records_for_month(incomes, date))))
 
 
-def expenses_over_time(expenses, incomes, column):
+def over_time(expenses, incomes, column):
     """Computes expenses distribution over time, for all months available in expenses"""
     post_describe_fn = None
     if column == 'amount #':
         post_describe_fn = lambda x: x['amount #'] * -1
     else:
         post_describe_fn = lambda x: x[column]
-    return _expenses_over_time_(expenses, incomes, post_describe_fn)
+    return describe_over_time(expenses, incomes, post_describe_fn)
