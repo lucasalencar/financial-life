5622ac2cba143747231ea2cd362efbf6df34e038
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-09-17 21:05:36 -0300

    Refactor to make plot distribution easier for new columns

--- investments/distribution.py
+++ investments/distribution.py
@@ -5,15 +5,15 @@ from investments import totals as tt
 from plotting import defaults
 import record_summary as rs
 
-def describe(invest, base_date):
+def describe(invest, column, base_date):
     """Returns relevant data about types"""
     current_month = rs.records_for_month(invest, base_date)
-    invested_by_type = tt.total_invested_by('type', current_month)
+    invested_by_type = tt.total_invested_by(column, current_month)
     return (invested_by_type / invested_by_type.sum()).sort_values('amount')
 
 
-def plot(invest, base_date, **configs):
+def plot(invest, column, base_date, **configs):
     """Plots pie chart with types distribution"""
-    by_type = describe(invest, base_date)
+    by_type = describe(invest, column, base_date)
     data = [go.Pie(labels=by_type.index, values=by_type.amount)]
     return iplot(defaults.figure(data, height=400, **configs))
