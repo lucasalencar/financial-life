c36264c43c2535bb2a42a99d9e99bf33813a4245
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2020-04-11 14:18:02 -0300

    Add pie plot to expenses distribution

---
+++ plotting/pie.py
@@ -0,0 +1,12 @@
+"""Pie plot related functions"""
+
+import plotly.graph_objs as go
+from plotly.offline import iplot
+
+from plotting import defaults
+
+
+def plot(series, **configs):
+    """Pie plot given a series"""
+    plot_data = [go.Pie(labels=series.index, values=series.values)]
+    return iplot(defaults.figure(plot_data, **configs))
