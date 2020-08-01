1d5c4c08a6c9028d24fb9d979dfafabebc6d809d
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2020-08-01 15:13:12 -0300

    Add index to investments that are already going and rename them

--- load/easynvest_fixed_term.py
+++ load/easynvest_fixed_term.py
@@ -15,9 +15,21 @@ def add_title(fixed_term):
     # Fix description to first letter capitalized
     fixed_term['description'] = fixed_term.description.str.title()
     # Add title based on type and description
-    fixed_term['title'] = fixed_term.type + ' ' + fixed_term.description
+    fixed_term['title'] = fixed_term.type + ' ' + fixed_term.description + ' ' + fixed_term['index']
     renamings = {
-        'CDB Voiter Banco Indusval': 'CDB Banco Indusval',
+        'CDB Voiter Banco Indusval 130% do CDI': 'CDB Banco Indusval',
+        'CDB Banco Indusval 130% do CDI': 'CDB Banco Indusval',
+        'CDB Banco Maxima 107% do CDI': 'CDB Banco Maxima',
+        'CDB Banco Maxima 114% do CDI': 'CDB Banco Maxima',
+        'CDB Banco Maxima 116% do CDI': 'CDB Banco Maxima',
+        'CDB Banco Maxima 117% do CDI': 'CDB Banco Maxima',
+        'CDB Banco Maxima 119% do CDI': 'CDB Banco Maxima',
+        'CDB Banco Maxima 120% do CDI': 'CDB Banco Maxima',
+        'CDB Banco Maxima 124% do CDI': 'CDB Banco Maxima',
+        'CDB Banco Maxima 128% do CDI': 'CDB Banco Maxima',
+        'LCI Banco Maxima 107% do CDI': 'LCI Banco Maxima',
+        'LCA Banco Maxima 100% do CDI': 'LCA Banco Maxima',
+        'LCA Banco Maxima 101% do CDI': 'LCA Banco Maxima',
     }
     return fixed_term.replace(renamings)
 
@@ -25,7 +37,6 @@ def add_title(fixed_term):
 def add_accounts(easynvest):
     account_mapping = {
         'CDB Banco Indusval':        'Magnetis',
-        'CDB Voiter Banco Indusval': 'Magnetis',
         'CDB Banco Maxima':          'Magnetis',
         'LCA Banco Maxima':          'Magnetis',
         'LCI Banco Maxima':          'Magnetis',
@@ -39,7 +50,6 @@ def add_accounts(easynvest):
 def add_goals(easynvest):
     goals_mapping = {
         'CDB Banco Indusval':        'Aposentadoria',
-        'CDB Voiter Banco Indusval': 'Aposentadoria',
         'CDB Banco Maxima':          'Aposentadoria',
         'LCA Banco Maxima':          'Aposentadoria',
         'LCI Banco Maxima':          'Aposentadoria',
