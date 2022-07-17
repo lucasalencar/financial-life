7416f4463ef7ebae2b4a64a370ff406b3f1ce085
Lucas Alencar <alencar.lucas.a@gmail.com>
2022-07-17 16:33:57 -0300

    Move datetime filters to a single namespace

--- src/filters/datetime.py
+++ src/filters/datetime.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import calendar
 
 from .. import date_helpers as dth
+
+
+def by_monthly_period(data, start, end):
+    """Filter data from the beggining of month on start until the end of month on end."""
+    month_last_day = calendar.monthrange(end.year, end.month)[1]
+    return data[(data.date >= pd.Timestamp(start.replace(day=1))) &
+                (data.date <= pd.Timestamp(end.replace(day=month_last_day)))]
+
 
 def records_for_month(records, base_date):
     """Selects records that goes from the beginning to end of month."""
