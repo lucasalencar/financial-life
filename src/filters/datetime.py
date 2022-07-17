3fb2ef5d938a729722b6a0c2b5579cb883d6867b
Lucas Alencar <alencar.lucas.a@gmail.com>
2022-07-17 17:57:12 -0300

    Refactor function to filter records by period monthly

--- src/filters/datetime.py
+++ src/filters/datetime.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import calendar
 
-from .. import date_helpers as dth
-
 
 def by_monthly_period(data, start, end):
     """Filter data from the beggining of month on start until the end of month on end."""
+    start_period_beginning_of_month = start.replace(day=1)
     month_last_day = calendar.monthrange(end.year, end.month)[1]
-    return data[(data.date >= pd.Timestamp(start.replace(day=1))) &
-                (data.date <= pd.Timestamp(end.replace(day=month_last_day)))]
+    end_period_end_of_month = end.replace(day=month_last_day)
+    return data[(data.date >= pd.Timestamp(start_period_beginning_of_month)) &
+                (data.date <= pd.Timestamp(end_period_end_of_month))]
