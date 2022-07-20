8b9766ab3c5dc992455a9a9e574e4c0ab94d031a
Lucas Alencar <alencar.lucas.a@gmail.com>
2022-07-20 16:36:56 -0300

    Remove unused function

--- src/aggregate/amount.py
+++ src/aggregate/amount.py
@@ -5,7 +5,3 @@ def total_amount_by(groupby, records):
     """Sums total amount given a column to group by"""
     return records.groupby(groupby).sum()
 
-
-def cumulative_amount_by(groupby, records):
-    """Cumulative total amount given a column to group by"""
-    return total_amount_by(groupby, records).sort_index().cumsum()
